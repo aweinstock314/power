3abefab61a178fd6a2b175445912b4768a4b6dd5
Avi Weinstock <aweinstock314@gmail.com>
2017-04-13 12:02:28 -0400

    Add simplified working example to examples/rayon_test.

--- examples/rayon_test.rs
+++ examples/rayon_test.rs
@@ -9,35 +9,47 @@ use rayon::iter::ParallelIterator;
 use rayon::iter::IntoParallelIterator;
 
 fn buggy() {
-    let noprogress = rayon::spawn_future_async(futures::future::lazy(move || {
+    let slow = rayon::spawn_future_async(futures::future::lazy(move || {
      Ok((0..(0u64).wrapping_sub(1)).into_par_iter().reduce(|| 0u64, |x, y|
          // without black_box, LLVM optimizes away the parallel iterator and returns 9223372036854775809
          test::black_box(x.wrapping_add(y))))
     })).map_err(|()| /* type inference hack */ ());
-    let progress = futures::future::ok(42);
+    let fast = futures::future::ok(42);
     let mut core = tokio_core::reactor::Core::new().unwrap();
-    println!("{:?}", core.run(noprogress.select(progress).map(|(res, _selectnext)| res).map_err(|(err, _selectnext)| err)).unwrap());
+    println!("{:?}", core.run(slow.select(fast).map(|(res, _selectnext)| res).map_err(|(err, _selectnext)| err)).unwrap());
     // Expected: 100% chance of instant "42", barring pathological custom schedulers
     // Actual: 50% chance of instant "42", 50% chance of 100% CPU usage across all cores doing number crunching
 }
 
 fn working() {
     let (send, recv) = futures::sync::mpsc::channel(10);
-    let noprogress = rayon::spawn_future_async(futures::future::lazy(move || {
+    let slow = rayon::spawn_future_async(futures::future::lazy(move || {
         Ok((0..(0u64).wrapping_sub(1)).into_par_iter().reduce(|| 0u64, |x, y|
             // without black_box, LLVM optimizes away the parallel iterator and returns 9223372036854775809
             test::black_box(x.wrapping_add(y))))
     }).and_then(|res| send.send(res)));
-    let progress = futures::future::ok(42);
-    rayon::spawn_async(move || { let _ = noprogress.rayon_wait(); }); // this feels kind of hacky_redundant
+    let fast = futures::future::ok(42);
+    rayon::spawn_async(move || { let _ = slow.rayon_wait(); }); // this feels kind of hacky/redundant
     let mut core = tokio_core::reactor::Core::new().unwrap();
-    println!("{:?}", core.run(recv.into_future().map(|(a, _)| a.unwrap()).select(progress).map(|(res, _selectnext)| res).map_err(|(err, _selectnext)| err)).unwrap());
+    println!("{:?}", core.run(recv.into_future().map(|(a, _)| a.unwrap()).select(fast).map(|(res, _selectnext)| res).map_err(|(err, _selectnext)| err)).unwrap());
+}
+
+fn working_simplified() {
+    let (send, recv) = futures::sync::oneshot::channel();
+    rayon::spawn_async(move || {
+        let _ = send.send((0..(0u64).wrapping_sub(1)).into_par_iter().reduce(|| 0u64, |x, y|
+            // without black_box, LLVM optimizes away the parallel iterator and returns 9223372036854775809
+            test::black_box(x.wrapping_add(y))));
+    });
+    let fast = futures::future::ok(42);
+    let mut core = tokio_core::reactor::Core::new().unwrap();
+    println!("{:?}", core.run(recv.select(fast).map(|(res, _selectnext)| res).map_err(|(err, _selectnext)| err)).unwrap());
 }
 
 fn main() {
     // strangely, "buggy" seems to work correctly if "working" is run first, but has the 50% behavior if "working" is commented out, or if the order is swapped. maybe it's an initialization thing?
     rayon::initialize(rayon::Configuration::new()).unwrap(); // this doesn't seem to help though
-    //working();
+    //working_simplified();
     println!("-----");
     buggy();
 }
